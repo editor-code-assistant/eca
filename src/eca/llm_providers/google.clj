70c94be1ddba8fa95984ad2fd3ceec09aa05d4b1
Marlon Silva <contato@marlonjsilva.com.br>
2025-07-02 20:47:26 -0300

    Updating with current changes

--- src/eca/llm_providers/google.clj
+++ src/eca/llm_providers/google.clj
@@ -1,59 +1,73 @@
 (ns eca.llm-providers.google
   (:require
    [cheshire.core :as json]
-   [clojure.java.io :as io]
    [clojure.string :as str]
+   [clojure.java.io :as io]
+   [eca.llm-util :as llm-util]
    [eca.logger :as logger]
    [hato.client :as http]))
 
-(def ^:private logger-tag "[GOOGLE_GEMINI]")
+(set! *warn-on-reflection* true)
 
-(def ^:private url "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions")
+(def ^:private logger-tag "[GEMINI]")
 
-(defn ^:private raw-data->messages [data]
-  (let [{:keys [choices]} (json/parse-string data true)]
-    (map (fn [{:keys [delta finish_reason]}]
-           (cond-> {}
-             (:content delta) (assoc :message (:content delta))
-             finish_reason (assoc :finish-reason finish_reason)))
-         choices)))
+(def ^:private gemini-url "https://generativelanguage.googleapis.com")
+(def ^:private responses-path "/v1beta/models/$model:streamGenerateContent?alt=sse")
 
-(defn ^:private ->message [{:keys [role behavior context]} user-prompt]
-  (format "%s\n%s\n%s\nThe user is asking: '%s'"
-          role behavior context user-prompt))
+(defn ^:private url [path]
+  (format "%s%s"
+          (or (System/getenv "GEMINI_API_URL")
+              gemini-url)
+          path))
 
-(defn completion! [{:keys [model user-prompt context temperature api-key]
-                    :or {temperature 1.0}}
-                   {:keys [on-message-received on-error]}]
-  (let [messages [{:role "user" :content (->message context user-prompt)}]
-        body {:model model
-              :messages messages
-              :temperature temperature
-              :stream true}
-        api-key (or api-key
-                    (System/getenv "GOOGLE_GEMINI_API_KEY"))]
+
+(defn ^:private base-completion-request! [{:keys [rid body api-key on-error on-response]}]
+  (let [api-key (or api-key
+                    (System/getenv "GEMINI_API_URL"))
+        url (str/replace (url responses-path) "$model" (:model body))]
+    (logger/debug logger-tag (format "Sending input: '%s' instructions: '%s' tools: '%s' url: '%s'"
+                                     (:input body)
+                                     (:instructions body)
+                                     (:tools body)
+                                     url))
     (http/post
      url
-     {:headers {"Authorization" (str "Bearer " api-key)
+     {:headers {"x-goog-api-key" (str api-key)
                 "Content-Type" "application/json"}
       :body (json/generate-string body)
-      :throw-exceptions? false
+      :throw-exceptions? true
       :async? true
       :as :stream}
      (fn [{:keys [status body]}]
        (try
          (if (not= 200 status)
-           (do
-             (logger/warn logger-tag "Unexpected response status" status)
-             (on-error {:message (str "Google Gemini response status: " status)}))
+           (let [body-str (slurp body)]
+             (logger/warn logger-tag "Unexpected response status: %s body: %s" status body-str)
+             (on-error {:message (format "Gemini response status: %s body: %s" status body-str)}))
            (with-open [rdr (io/reader body)]
-             (doseq [line (line-seq rdr)]
-               (when (str/starts-with? line "data: ")
-                 (let [data (subs line 6)]
-                   (when-not (= "[DONE]" data)
-                     (doseq [message (raw-data->messages data)]
-                       (on-message-received message))))))))
+             (doseq [[event data] (llm-util/event-data-seq rdr)]
+               (llm-util/log-response logger-tag rid event data)
+               (on-response event data))))
          (catch Exception e
            (on-error {:exception e}))))
      (fn [e]
        (on-error {:exception e})))))
+
+(defn completion! [{:keys [model user-prompt context temperature api-key past-messages tools web-search]
+                    :or {temperature 1.0}}
+                   {:keys [on-message-received on-error on-tool-called on-reason]}]
+  (let [input (conj past-messages {:role "user" :parts [{:text user-prompt}]})
+        tools (cond-> tools
+                web-search (conj {:google_search {}}))
+        body {:model model
+              :contents input
+              :system_instruction {:parts [{:text context}]}
+              :tools tools
+              :stream true}
+        on-response-fn (fn handle-response [_event data]
+                         ())]
+    (base-completion-request!
+     {:body body
+      :api-key api-key
+      :on-error on-error
+      :on-response on-response-fn})))
