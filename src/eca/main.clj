4cf0e5bc8ac09c84f7e317573244590beecb8e07
Eric Dallo <ericdallo06@hotmail.com>
2026-02-19 11:19:04 -0300

    Fix cyclic dep for graal

--- src/eca/main.clj
+++ src/eca/main.clj
@@ -5,9 +5,10 @@
    [babashka.cli :as cli]
    [borkdude.dynaload]
    [clojure.string :as string]
-   [eca.config :as config]
-   [eca.logger :as logger]
    [eca.client-http :as client]
+   [eca.config :as config]
+   [eca.features.agents] ;; TODO how to fix this cyclic dep
+   [eca.logger :as logger]
    [eca.server :as server]))
 
 (set! *warn-on-reflection* true)
