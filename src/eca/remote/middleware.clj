e7ffe8c0d720a6038d2468fa8b6da65bb854a3fb
Eric Dallo <ericdallo06@hotmail.com>
2026-03-19 14:56:47 -0300

    Small fixes

--- src/eca/remote/middleware.clj
+++ src/eca/remote/middleware.clj
@@ -3,22 +3,39 @@
 
 (set! *warn-on-reflection* true)
 
-(def ^:private cors-headers
-  {"Access-Control-Allow-Origin" "https://web.eca.dev"
-   "Access-Control-Allow-Methods" "GET, POST, DELETE, OPTIONS"
-   "Access-Control-Allow-Headers" "Content-Type, Authorization"})
+(def ^:private allowed-origin "https://web.eca.dev")
+
+(defn- allowed-origin?
+  "Returns the origin if allowed, nil otherwise.
+   Allows web.eca.dev and localhost for development."
+  [origin]
+  (when origin
+    (cond
+      (= origin allowed-origin) origin
+      (re-matches #"http://localhost(:\d+)?" origin) origin
+      (re-matches #"http://127\.0\.0\.1(:\d+)?" origin) origin
+      :else nil)))
+
+(defn- cors-headers-for
+  [request]
+  (let [origin (get-in request [:headers "origin"])
+        resolved (or (allowed-origin? origin) allowed-origin)]
+    {"Access-Control-Allow-Origin" resolved
+     "Access-Control-Allow-Methods" "GET, POST, DELETE, OPTIONS"
+     "Access-Control-Allow-Headers" "Content-Type, Authorization"}))
 
 (defn wrap-cors
-  "Ring middleware adding CORS headers for web.eca.dev.
+  "Ring middleware adding CORS headers for web.eca.dev and localhost.
    Handles OPTIONS preflight with 204."
   [handler]
   (fn [request]
-    (if (= :options (:request-method request))
-      {:status 204
-       :headers cors-headers
-       :body nil}
-      (let [response (handler request)]
-        (update response :headers merge cors-headers)))))
+    (let [headers (cors-headers-for request)]
+      (if (= :options (:request-method request))
+        {:status 204
+         :headers headers
+         :body nil}
+        (let [response (handler request)]
+          (update response :headers merge headers))))))
 
 (defn wrap-json-content-type
   "Ring middleware that sets default JSON content-type on responses
